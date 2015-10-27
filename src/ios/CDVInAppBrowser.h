418b158a86545a135f5e722680fd16348cb49ac8
Funzing <Funzing@users.noreply.github.com>
2015-10-27 14:14:40 +0200

    Updates according to shimon's updates

--- src/ios/CDVInAppBrowser.h
+++ src/ios/CDVInAppBrowser.h
@@ -86,6 +86,8 @@
 @property (nonatomic, strong) IBOutlet UIWebView* webView;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* closeButton;
 @property (nonatomic, strong) IBOutlet UILabel* addressLabel;
+@property (nonatomic, strong) UILabel* titleLabel;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem* titleButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
